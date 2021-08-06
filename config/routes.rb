9f93aae8edc95a2610d776256a91e4297cce28da
Nicolas <ninfante@e-learning.com.co>
2021-08-05 21:55:46 -0500

    As a user I should be able to sign up (suggested changes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+=begin
   namespace :api do
     namespace :v1 do
       get 'registrations/sign_in_params'
     end
   end
+=end
   mount_devise_token_auth_for 'User', at: 'api/v1', controllers: {
     registrations: 'api/v1/registrations'
   }
